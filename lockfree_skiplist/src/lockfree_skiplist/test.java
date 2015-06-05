0f0400a987f9517d35c95fa1033795bf11531ec7
maneesh4848 <maneesh4848@gmail.com>
2015-06-05 21:12:49 +0530

    Changed for performance comparison

--- lockfree_skiplist/src/lockfree_skiplist/test.java
+++ lockfree_skiplist/src/lockfree_skiplist/test.java
@@ -1,34 +1,84 @@
 package lockfree_skiplist;
 
-
 public class test
 {
 	public static void main(String[] args)
-	{		
-		skiplisttest listtest = new skiplisttest();
-		//listtest.testSequential();
-		/*try
+	{
+		long [][] small,big;
+		small = new long[6][10];
+		big = new long[6][5];
+
+		for(int i = 0; i < 5; i++)
 		{
-			listtest.testParallelAdd();
+			for(int j = 0; j < 10; j++)
+			{
+				System.out.println(i);
+				System.out.println(j);
+				skiplisttest listtest = new skiplisttest(i,0);
+				try
+				{
+					small[i][j] = listtest.testParallelBoth();
+				}
+				catch (Exception e)
+				{
+				}
+			}
 		}
-		catch (Exception e)
+		/*for(int i = 0; i < 10; i++)
 		{
-		}
-		try
-		{
-			listtest.testParallelRemove();
-		}
-		catch (Exception e1)
-		{
+			skiplisttest listtest = new skiplisttest(6,0);
+			try
+			{
+				small[6][i] = listtest.testContains();
+			}
+			catch (Exception e)
+			{
+			}
 		}*/
-		try
+
+		long avg[] = new long[6];
+		for(int i = 0; i < 5; i++)
 		{
-			listtest.testParallelBoth();
+			//System.out.println(Arrays.asList(small[i]));
+			for(int j = 0; j < 10; j++)
+			{
+				System.out.println(Integer.toString(i) + " " + Integer.toString(j) + " " + Long.toString(small[i][j]));
+				avg[i] += small[i][j];
+			}
+			avg[i] /= 10;
+			System.out.println(Integer.toString(i) + " " + Long.toString(avg[i]));
 		}
-		catch (Exception e)
+		//System.out.println(Arrays.asList(avg));
+		
+		for(int i = 0; i < 5; i++)
 		{
+			for(int j = 0; j < 5; j++)
+			{
+				skiplisttest listtest = new skiplisttest(i,1);
+				try
+				{
+					big[i][j] = listtest.testParallelBoth();
+				}
+				catch (Exception e)
+				{
+				}
+			}
 		}
 		
+		long avg2[] = new long[6];
+		for(int i = 0; i < 5; i++)
+		{
+			//System.out.println(Arrays.asList(big[i]));
+			for(int j = 0; j < 5; j++)
+			{
+				System.out.println(Integer.toString(i) + " " + Integer.toString(j) + " " + Long.toString(big[i][j]));
+				avg2[i] += big[i][j];
+			}
+			avg2[i] /= 10;
+			System.out.println(Integer.toString(i) + " " + Long.toString(avg2[i]));
+		}
+		//System.out.println(Arrays.asList(avg2));
+
 		System.exit(0);
 	}
 }
